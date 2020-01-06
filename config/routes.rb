6ca8e7d43907b9b02dba201a28940635c9125ff3
jemmeli <jemmeli84@gmail.com>
2020-01-06 20:06:00 +0100

    add pages and view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
+  #root 'pages#home'
+  get 'home' , to: 'pages#home'
   get 'about' , to: 'pages#about'
 
 end
